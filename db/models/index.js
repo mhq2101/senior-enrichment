8d952174e86941de578c91939fb4add5c8179fb7
Mikhail Qader <mhq2101@columbia.edu>
2017-07-09 14:35:37 -0400

    models

--- db/models/index.js
+++ db/models/index.js
@@ -5,6 +5,13 @@
 	// This works if we all use the same Sequelize instance (instantiated in and exported from `/db/index.js`)
 	// This is an acceptable pattern but it does have limitations in that if you change the name of the model you will have to change every time it is requeired everywhere
 
-const User = require('./user')
+const Student = require('./Student');
+const Campus = require('./Campus');
 
-module.exports = {User}
+Student.belongsTo(Campus, { as: 'campus' });
+Campus.hasMany(Student);
+
+module.exports = {
+	Student,
+	Campus
+}
